09094616d56885842073c61fa61d77b21cc9fb2d
Lily <ngross1015@gmail.com>
2025-11-03 15:51:42 -0800

    2015 Day 2 Part Two Python Complete

--- 2015/Python/Day 2/solution.py
+++ 2015/Python/Day 2/solution.py
@@ -1,5 +1,6 @@
 presentDimensions = []
 surfaceArea = 0
+ribbon = 0
 
 with open("input.txt") as f:
     for line in f :
@@ -8,6 +9,10 @@ with open("input.txt") as f:
 for i in presentDimensions:
     dimensions = i.split("x")
     surfaceArea += (2 * (int(dimensions[0]) * int(dimensions[1])) + 2 * (int(dimensions[1]) * int(dimensions[2])) + 2 * (int(dimensions[2]) * int(dimensions[0]))) + min([int(dimensions[0]) * int(dimensions[1]), int(dimensions[1]) * int(dimensions[2]), int(dimensions[2]) * int(dimensions[0])])
+    
+    ribbon += min([2 * int(dimensions[0]) + 2 * int(dimensions[1]), 2 * int(dimensions[1]) + 2 * int(dimensions[2]), 2 * int(dimensions[2]) + 2 * int(dimensions[0])]) + (int(dimensions[0]) * int(dimensions[1]) * int(dimensions[2]))
+    
     dimensions = []
 
 print(surfaceArea)
+print(ribbon)
