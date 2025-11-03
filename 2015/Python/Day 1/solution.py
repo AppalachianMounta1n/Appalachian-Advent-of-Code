a1c17a4287513cb1225c6c2cf49690d7b0542929
Lily <ngross1015@gmail.com>
2025-11-03 13:53:36 -0800

    2015 Day 1 Part 2 - Python Complete

--- 2015/Python/Day 1/solution.py
+++ 2015/Python/Day 1/solution.py
@@ -1,7 +1,18 @@
 floor = 0
+instructions = ""
 
 with open('input.txt') as f:
     for line in f:
-        floor += line.count('(') - line.count(')')
+        instructions += line
+        
+    floor += instructions.count('(') - instructions.count(')')
+    print(floor)
 
-print(floor)
+    floor = 0
+    basementFirst = 0
+    for i in instructions:
+        floor += 1 if i == '(' else -1
+        basementFirst -= -1
+        if floor == -1:
+            print("First basement: " + str(basementFirst))
+            break
